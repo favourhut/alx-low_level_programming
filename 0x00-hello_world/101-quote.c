8db4935d8104ad2e3284893c5592a2357c1f269b
Favour <favourjames061@gmail.com>
2023-09-03 15:41:40 -0700

    standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - this program prints to standard error
+ * Return: 1 (error)
+ */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\"- Dora Korpar, 2015-10-19");
+	return (1);
+}
