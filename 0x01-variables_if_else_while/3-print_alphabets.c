975176424590364fc7ae05ea5296275dc0e18a3e
Favour <favourjames061@gmail.com>
2023-09-09 00:31:34 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - alphabet in lowercase, and then in uppercase
+ * Return: (0) Success
+ */
+int main(void)
+{
+	char letter = 'a';
+	char uppercase = 'A';
+
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		letter++;
+	}
+	while (uppercase <= 'Z')
+	{
+		putchar(uppercase);
+		uppercase++;
+	}
+	return (0);
+}
