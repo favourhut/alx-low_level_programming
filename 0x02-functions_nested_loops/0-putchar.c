63fcc38c272aba697c6677ec0627d869b1fb1531
Favour <favourjames061@gmail.com>
2023-09-12 07:25:24 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - print _putchar followed by a new line
+ * Return: (0) Success
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putcahr('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
