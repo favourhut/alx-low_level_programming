dfe23ed9ebdf42b2ef69ba0b2c8a8a3d7c6a921d
Favour <favourjames061@gmail.com>
2023-09-26 06:50:14 -0700

    update to  function that copies memory area

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * _memcpy the function that copies a memory area
- * @n variable to copy bytes from
+ * _memcpy - the function that copies a memory area
+ * @n: variable to copy bytes from
  * @src: bytes from memory area
  * @dest: copy to memory area
  * Return: a pointer to dest
