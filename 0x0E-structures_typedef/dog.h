510a5810b105b6711520300aaf84a924dcac26f5
Favour <favourjames061@gmail.com>
2023-10-10 14:09:11 -0700

    updated the betty style and comment

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,12 @@
-#ifndef MY_HEADER_H
-#define MY_HEADER_H
+#ifndef HEADER_H
+#define HEADER_H
+
+/**
+ * struct dog - structure with dog type
+ * @name: name of hte dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
 
 struct dog
 {
