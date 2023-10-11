f53042a4f16b4fc5067bae2b4cde4fa3ab9dc873
Favour <favourjames061@gmail.com>
2023-10-10 21:20:29 -0700

    Write a function that prints a struct dog using printf and the / symbol

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * print_dog - this function printsa struct dog
+ * @d: ponter variable to struct dog
+ *
+ * Return: nothing
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		printf("Name: %s\n", (d->name) ? d->name : "(nil)");
+		printf("Age: %f\n", (d->age) ? d->age : 0);
+		printf("Owner:%s\n", (d->owner) ? d->owner : "(nil)");
+	}
+}
+
