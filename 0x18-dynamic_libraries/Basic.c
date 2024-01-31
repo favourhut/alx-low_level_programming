6805beab435f25f5d833732c7751ca25d631f79c
Favour James <favourjames061@gmail.com>
2024-01-31 14:13:48 -0800

    Callng c function from python

---
+++ 0x18-dynamic_libraries/Basic.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+/**
+ * add - this functions adds integers
+ * @a: integer to add
+ * @b: interger to add
+ * Return: success
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - This function substract
+ * @a: firdt int
+ * @b: second int
+ * Return: succes
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - this function multiplies
+ * @a: int to multi 1
+ * @b: int to multipy 2
+ * Return: success
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - this funcntion divides
+ * @a: first to divide
+ * @b: second to divide
+ * Return - success
+ */
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - This function returns the remainder of a given calculation
+ * @a: First integer
+ * @b: secound integer
+ * Return: success
+ */
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
---
+++ 0x18-dynamic_libraries/mod.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * mod - This function returns the remainder of a given calculation
+ * @a: First integer
+ * @b: secound integer
+ * Return: success
+ */
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
