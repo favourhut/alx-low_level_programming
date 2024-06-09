e7d88c71cb183dc7cd6df27d8e9a3203e6dff6e3
Favour James <Favourjames061@gmail.com>
2024-06-08 18:33:05 -0700

    UNIX is basically a simple operating system

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,8 +3,8 @@
  * main - this program prints to standard error
  * Return: 1 (error)
  */
-int main()
+int main(void)
 {
-fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
